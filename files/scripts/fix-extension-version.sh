894e903619430397d04e7c82b18bd4b4311a5ca6
Ian Santos <48574278+iansantosdev@users.noreply.github.com>
2025-10-26 01:37:11 -0300

    fix(script): update sp-tray version support

--- files/scripts/fix-extension-version.sh
+++ files/scripts/fix-extension-version.sh
@@ -3,4 +3,4 @@
 set -oue pipefail
 
 FILE=/usr/share/gnome-shell/extensions/sp-tray@sp-tray.esenliyim.github.com/metadata.json
-jq '.["shell-version"] += ["48"]' $FILE > tmp && mv tmp $FILE
+jq '.["shell-version"] += ["49"]' $FILE > tmp && mv tmp $FILE
