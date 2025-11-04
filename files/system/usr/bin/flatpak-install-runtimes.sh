8b89e188355a62145d5b3878ec5da452de89da2e
Ian Santos <48574278+iansantosdev@users.noreply.github.com>
2025-11-04 00:15:53 -0300

    refactor(recipe): remove flatpak-install-runtimes service

--- files/system/usr/bin/flatpak-install-runtimes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -oue pipefail
-
-flatpak install --user --noninteractive runtime/org.gtk.Gtk3theme.Adwaita-dark/x86_64/3.22
-
-mkdir -p "$HOME/.config"
-touch "$HOME/.config/flatpak-install-runtimes-done"
