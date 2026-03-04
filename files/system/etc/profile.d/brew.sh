bcef10347bd2c3f792a23a77b85b79148de70751
Ian Santos <48574278+iansantosdev@users.noreply.github.com>
2026-03-04 03:35:34 -0300

    Add check for existing Homebrew prefix
    This change ensures that Homebrew environment variables are only set if
    the specified prefix directory actually exists. It also prevents
    duplicate entries in the PATH by checking for their existence before
    prepending.

--- files/system/etc/profile.d/brew.sh
+++ files/system/etc/profile.d/brew.sh
@@ -1,6 +1,15 @@
-export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
-export HOMEBREW_CELLAR="$HOMEBREW_PREFIX/Cellar"
-export HOMEBREW_REPOSITORY="$HOMEBREW_PREFIX/Homebrew"
-export PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin${PATH:+:$PATH}"
-[ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}"
-export INFOPATH="$HOMEBREW_PREFIX/share/info:${INFOPATH:-}"
+HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
+
+if [ -d "$HOMEBREW_PREFIX" ]; then
+    export HOMEBREW_PREFIX="$HOMEBREW_PREFIX"
+    export HOMEBREW_CELLAR="$HOMEBREW_PREFIX/Cellar"
+    export HOMEBREW_REPOSITORY="$HOMEBREW_PREFIX/Homebrew"
+
+    case ":$PATH:" in
+        *":$HOMEBREW_PREFIX/bin:"*) ;;
+        *) export PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin${PATH:+:$PATH}" ;;
+    esac
+
+    export MANPATH="$HOMEBREW_PREFIX/share/man${MANPATH+:$MANPATH}:"
+    export INFOPATH="$HOMEBREW_PREFIX/share/info:${INFOPATH:-}"
+fi
