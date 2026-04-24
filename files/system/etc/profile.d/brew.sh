e0cfbaab40d9616aee80e5a871d85a4b17c8e8a7
Ian Santos <48574278+iansantosdev@users.noreply.github.com>
2026-04-24 05:04:48 -0300

    fix(homebrew): skip profile setup for root

--- files/system/etc/profile.d/brew.sh
+++ files/system/etc/profile.d/brew.sh
@@ -1,6 +1,8 @@
+# shellcheck shell=bash
+
 HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
 
-if [ -d "$HOMEBREW_PREFIX" ]; then
+if [ "$(id -u)" -ne 0 ] && [[ -d "$HOMEBREW_PREFIX" ]]; then
     export HOMEBREW_PREFIX="$HOMEBREW_PREFIX"
     export HOMEBREW_CELLAR="$HOMEBREW_PREFIX/Cellar"
     export HOMEBREW_REPOSITORY="$HOMEBREW_PREFIX/Homebrew"
