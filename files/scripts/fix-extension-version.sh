37fb31336b022845bed4f611b1881df25017d408
Ian Santos <48574278+iansantosdev@users.noreply.github.com>
2026-04-24 05:08:18 -0300

    feat(gnome): support GNOME Shell 50

--- files/scripts/fix-extension-version.sh
+++ files/scripts/fix-extension-version.sh
@@ -3,4 +3,7 @@
 set -oue pipefail
 
 FILE=/usr/share/gnome-shell/extensions/sp-tray@sp-tray.esenliyim.github.com/metadata.json
-jq '.["shell-version"] += ["49"]' $FILE > tmp && mv tmp $FILE
+jq '.["shell-version"] += ["50"]' $FILE > tmp && mv tmp $FILE
+
+FILE=/usr/share/gnome-shell/extensions/clipboard-history@alexsaveau.dev/metadata.json
+jq '.["shell-version"] += ["50"]' $FILE > tmp && mv tmp $FILE
